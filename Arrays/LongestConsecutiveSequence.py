a7cc2cf6304be09f19c73cbe9cafc16e6647b3c3
Indika <45280177+IndiW@users.noreply.github.com>
2023-09-26 23:43:11 -0400

    Update LongestConsecutiveSequence.py

--- Arrays/LongestConsecutiveSequence.py
+++ Arrays/LongestConsecutiveSequence.py
@@ -9,10 +9,24 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 Input: nums = [0,3,7,2,5,8,4,6,0,1]
 Output: 9
 
-0 <= nums.length <= 105
--109 <= nums[i] <= 109
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
 
 '''
 
-def longestConsecutive(self, nums) -> int:
-    return []
+def longestConsecutive(nums) -> int:
+    unique = set(nums)
+    longest = 0
+    for n in nums:
+        curr = n
+        currLength = 1
+        while (curr + 1) in unique:
+            currLength += 1
+            curr += 1
+        longest = max(longest, currLength)
+
+
+    return longest
+
+
+print(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
