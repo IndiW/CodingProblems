13a7f11972e9985e24d367f2991425ed63fbcdc9
Indika <45280177+IndiW@users.noreply.github.com>
2023-10-22 10:51:30 -0400

    Update stackProblems.py

--- Stack/stackProblems.py
+++ Stack/stackProblems.py
@@ -67,3 +67,58 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.stack[-1][1]
+
+
+'''
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+    The valid operators are '+', '-', '*', and '/'.
+    Each operand may be an integer or another expression.
+    The division between two integers always truncates toward zero.
+    There will not be any division by zero.
+    The input represents a valid arithmetic expression in a reverse polish notation.
+    The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+
+
+'''
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operations = {
+            '+': lambda a, b: a + b, 
+            '-': lambda a, b: b - a, 
+            '/': lambda a, b: int(b/a), 
+            '*': lambda a, b: a * b
+            }
+        for s in tokens:
+            if s in operations:
+                if len(stack) < 2:
+                    return -1
+                op1 = stack.pop(-1)
+                op2 = stack.pop(-1)
+                stack.append(operations[s](op1, op2))
+            else:
+                stack.append(int(s))
+        
+        return stack.pop()
