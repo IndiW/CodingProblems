f4d304e8ad964761f3bcd0cf5a37bc92f8bd0886
Indika <45280177+IndiW@users.noreply.github.com>
2023-10-28 18:32:11 -0400

    Update binarySearchProblems.py

--- BinarySearch/binarySearchProblems.py
+++ BinarySearch/binarySearchProblems.py
@@ -66,5 +66,51 @@ class Solution:
         return False
         
         
-                
+'''
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+'''        
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        '''
+        k such that sum(time to all the piles) <= h
+        
+        have to try numbers from 1 to max(piles)
+        can use binSearch to find the number
+        '''
+        # we can only eat at most 1 pile per hour
+        if (len(piles)) > h:
+            return -1
+        
+        def validPile(k, h):
+            time_to_eat = 0
+            for pile in piles:
+                time_to_eat += math.ceil(pile / k)
+            return time_to_eat <= h
+        
+        lo = 1
+        hi = max(piles)
+        while lo < hi:
+            mid = lo + ((hi - lo) // 2)
+            if validPile(mid, h):
+                hi = mid
+            else:
+                lo = mid + 1
+        return hi
+
+
+
+        
             
+
+
+
+
+
