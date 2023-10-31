6331e9715ac7feccd59e9ee34aa7f930d056e06b
Indika <45280177+IndiW@users.noreply.github.com>
2023-10-31 08:28:42 -0400

    Update binarySearchProblems.py

--- BinarySearch/binarySearchProblems.py
+++ BinarySearch/binarySearchProblems.py
@@ -200,7 +200,51 @@ Implement the TimeMap class:
 '''
             
 
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+        self.timestampStore = {}
         
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.store:
+            self.store[key][timestamp] = value
+            self.timestampStore[key].append(timestamp)
+        else:
+            self.store[key] = { timestamp: value }
+            self.timestampStore[key] = [timestamp]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+        else:
+            nums = self.timestampStore[key]
+            target = timestamp
+            lo = 0
+            hi = len(nums) - 1
+            prevTime = -1
+            while lo <= hi:
+                mid = lo + (hi - lo) // 2
+                if nums[mid] > target:
+                    hi = mid - 1
+                elif nums[mid] < target:
+                    prevTime = nums[mid]
+                    lo = mid + 1
+                else:
+                    return self.store[key][nums[mid]]
+            if prevTime == -1:
+                return ""
+            
+            return self.store[key][prevTime] 
+
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
             
 
 
