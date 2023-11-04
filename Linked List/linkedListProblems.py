6701c583ab4a545713301b3007fcbccd73a3171a
Indika <45280177+IndiW@users.noreply.github.com>
2023-11-04 18:42:51 -0400

    Update linkedListProblems.py

--- Linked List/linkedListProblems.py
+++ Linked List/linkedListProblems.py
@@ -49,6 +49,58 @@ class Solution:
             head0.next = head2
         return start.next
 
+'''
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        
+        # find middle of linked list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # slow is at middle of list, start of 2nd list
+        # reverse second list
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        
+        # prev is start of new reversed list
+    
+        # merge two lists
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
+
+          
+
+
+
+
+
 
 
         
