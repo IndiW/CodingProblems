8cebfc3271da49c22ef5e4179a1f4b8eeaa8bb7d
Indika <45280177+IndiW@users.noreply.github.com>
2023-11-08 12:00:32 -0500

    Update linkedListProblems.py

--- Linked List/linkedListProblems.py
+++ Linked List/linkedListProblems.py
@@ -167,7 +167,50 @@ class Solution:
 
 
 
+'''
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+'''
 
         
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        head = res = ListNode(0)
+        while l1 or l2:
+            if l1 and l2:
+                s = l1.val + l2.val + carry
+                l1 = l1.next
+                l2 = l2.next
+            elif l1:
+                s = l1.val + carry
+                l1 = l1.next
+            else:
+                s = l2.val + carry
+                l2 = l2.next
+            if s < 10:
+                res.next = ListNode(s)
+                res = res.next
+                carry = 0
+            else:
+                res.next = ListNode(s%10)
+                res = res.next
+                carry = 1
         
+        if carry:
+            res.next = ListNode(carry)
+        return head.next
+        
+
+
+
+            
+        
+        
+            
