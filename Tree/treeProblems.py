63953b53f2f4ac48c7fc39c94e7602786e6edd8a
Indika <45280177+IndiW@users.noreply.github.com>
2023-11-26 22:54:28 -0500

    Update treeProblems.py

--- Tree/treeProblems.py
+++ Tree/treeProblems.py
@@ -331,3 +331,26 @@ class Solution:
         
         return validate(root)
                 
+
+'''
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+The following solution is an inorder traversal of the tree
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        def toArr(root):
+            if not root:
+                return []
+            else:
+                return toArr(root.left) + [root] + toArr(root.right)
+        
+        arr = toArr(root)
+        return arr[k-1].val
