9a947c97dcfb2d834b47630a26519289f89772fc
Indika <45280177+IndiW@users.noreply.github.com>
2023-12-01 23:30:49 -0500

    Update dp.py

--- DynamicProgramming/dp.py
+++ DynamicProgramming/dp.py
@@ -4,3 +4,18 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 '''
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # base case: O(0) = 0, O(1) = 1, O(2) = 2
+        # recursive case: O(n) = O(n-1) + 1 + O(n-2) + 2
+        memo = {}
+        d = [0,1,2]
+        # O(n) = O(n-1) + O(n-2) + 1
+        if n < len(d):
+            return d[n]
+        else:
+            for i in range(3, n+1):
+                d.append(d[i-1] + d[i-2])        
+        
+        return d[-1]
+
