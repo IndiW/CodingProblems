3cc3345a8c0eee53d7d43d0e7d107e7b4225c242
Indika <45280177+IndiW@users.noreply.github.com>
2023-12-06 08:49:31 -0500

    Update graphProblems.py

--- Graphs/graphProblems.py
+++ Graphs/graphProblems.py
@@ -51,3 +51,36 @@ An adjacency list is a collection of unordered lists used to represent a finite 
 
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 '''
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+        seen = { node.val: Node(node.val)}
+
+        q = [node]
+        while q:
+            curr = q.pop(-1)
+            curr_clone = seen[curr.val]
+            for nbr in curr.neighbors:
+                if nbr.val not in seen:
+                    seen[nbr.val] = Node(nbr.val)
+                    q.append(nbr)
+                curr_clone.neighbors.append(seen[nbr.val])
+
+        return seen[node.val]
+
+
+
+
+        
