c5acf044cfd9fac2415608fdb3aec1f8dde0b557
Indika <45280177+IndiW@users.noreply.github.com>
2023-12-14 20:19:52 -0500

    Update heapAndPq.py

--- Heap/heapAndPq.py
+++ Heap/heapAndPq.py
@@ -42,3 +42,27 @@ Implement KthLargest class:
 
 '''
 
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.pool = nums
+        self.k = k
+        heapq.heapify(self.pool)
+        while len(self.pool) > k:
+            # remove elements until we only have k elements
+            heapq.heappop(self.pool)
+        
+
+    def add(self, val: int) -> int:
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
