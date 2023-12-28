82a2c791f33a2ac4c6260f26ac5fe390fd09bd8c
Indika <45280177+IndiW@users.noreply.github.com>
2023-12-27 23:31:50 -0500

    Update greedy.py

--- Greedy/greedy.py
+++ Greedy/greedy.py
@@ -14,12 +14,12 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        dp = [nums[0]]
-        max_val = dp[0]
+        prev = nums[0]
+        max_val = prev
         for i in range(1, len(nums)):
-            if dp[i-1] > 0:
-                dp.append(dp[i-1] + nums[i])
+            if prev > 0:
+                prev = prev + nums[i]
             else:
-                dp.append(nums[i])
-            max_val = max(max_val, dp[i])
+                prev = nums[i]
+            max_val = max(max_val, prev)
         return max_val
