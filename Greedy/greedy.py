46b907483dfccb3ac3f7677a4ffff896f00b7521
Indika <45280177+IndiW@users.noreply.github.com>
2023-12-28 23:53:30 -0500

    Update greedy.py

--- Greedy/greedy.py
+++ Greedy/greedy.py
@@ -23,3 +23,12 @@ class Solution:
                 prev = nums[i]
             max_val = max(max_val, prev)
         return max_val
+
+
+'''
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+'''
