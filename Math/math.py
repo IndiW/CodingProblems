908484d7a02861fafcf6ef10b5ab1058613ece72
Indika <45280177+IndiW@users.noreply.github.com>
2024-01-09 23:54:07 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -29,3 +29,10 @@ class Solution:
             if s == 1:
                 return True
             n = str(s)
+
+
+'''
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+'''
