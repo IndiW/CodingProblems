e9a2bc9bad1219e36c06b29689ae1a7be080f035
Indika <45280177+IndiW@users.noreply.github.com>
2024-01-12 00:01:48 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -30,6 +30,32 @@ class Solution:
                 return True
             n = str(s)
 
+'''
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+'''
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if not digits:
+            return digits
+        digits[-1] += 1
+        i = -1
+        while digits[i] >= 10:
+            digits[i] = digits[i] % 10
+            i -= 1
+            if i < -len(digits):
+                digits = [1] + digits
+                return digits
+            else:
+                digits[i] += 1
+        
+        return digits
+
+
+
+        
 
 '''
 You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
