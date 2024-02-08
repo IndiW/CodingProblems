89f51f769d2a320f02af4f80e94bdbf3c0640544
Indika <45280177+IndiW@users.noreply.github.com>
2024-02-08 12:46:20 -0500

    Update dp.py

--- DynamicProgramming/dp.py
+++ DynamicProgramming/dp.py
@@ -153,4 +153,28 @@ class Solution:
     
 
 
+'''
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+'''      
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # OPT(amount) = min(OPT(amount - coin) for coin in coins)
+        dp = [0]
+        for i in range(1, amount+1):
+            counts = []
+            for coin in coins:
+                if len(dp) >= coin and dp[i-coin] >= 0:
+                    counts.append(dp[i-coin] + 1)
+                else:
+                    counts.append(float('inf'))
+            dp.append(min(counts))
+        
+        if dp[-1] == float('inf'):
+            return -1
+        return dp[-1]
         
