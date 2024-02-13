2dbcf14cdf746ce49373eb84855e9aa755b0ed1a
Indika <45280177+IndiW@users.noreply.github.com>
2024-02-12 21:27:45 -0500

    Update math.py

--- Math/math.py
+++ Math/math.py
@@ -116,6 +116,25 @@ class Solution:
         
         return dp(digits)
         
+#Iterative solution:
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        length = len(digits) - 1
+        while digits[length] == 9:
+            digits[length] = 0
+            length -= 1
+        if length < 0:
+            digits = [1] + digits
+        else:
+            digits[length] += 1
+        
+        return digits
+        
+
+
+
+
+        
 
 
 
