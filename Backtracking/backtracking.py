4b510252ef61498032d996c663cdb882ea9659bc
Indika <45280177+IndiW@users.noreply.github.com>
2024-02-28 08:05:14 -0500

    Update backtracking.py

--- Backtracking/backtracking.py
+++ Backtracking/backtracking.py
@@ -155,6 +155,29 @@ class Solution:
         return False
 
 
+'''
+Given a string s, partition s such that every
+substring
+of the partition is a
+palindrome
+. Return all possible palindrome partitioning of s.
+'''
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        self.dfs(s,[],res)
+        return res
+
+    
+    def dfs(self,s,path,res):
+        if not s:
+            res.append(path)
+            return
+        for i in range(1, len(s)+1):
+            if s[:i] == s[:i][::-1]:
+                self.dfs(s[i:], path+[s[:i]], res)
+            
 
 
         
