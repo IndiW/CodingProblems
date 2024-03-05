8ce85a118e07a14ca6d5f591d24ee5e41820bb35
Indika <45280177+IndiW@users.noreply.github.com>
2024-03-05 07:50:36 -0500

    Update graphProblems.py

--- Graphs/graphProblems.py
+++ Graphs/graphProblems.py
@@ -457,4 +457,65 @@ Add system design problems to this repo?
 
 '''
 
+'''
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+    Every adjacent pair of words differs by a single letter.
+    Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+    sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+'''
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # bfs from beginWord to endWord
+        # construct a graph - adj matrix out of wordList        
+        def bfs(beginWord, endWord):
+            visited = set()
+            q = [(beginWord, 1)]
+            while q:
+                word, count = q.pop(0)
+                if word not in visited:
+                    visited.add(word)
+                    if word == endWord:
+                        return count
+                    for i in range(len(word)):
+                        s = word[:i] + "_" + word[i+1:]
+                        neighbours = graph.get(s, [])
+                        for w in neighbours:
+                            if w not in visited:
+                                q.append((w, count+1))
+            return 0
+
+            
+                 
+        def differsByOne(s1, s2):
+            if len(s1) != len(s2):
+                return False
+            diffByOne = False
+            for i in range(len(s1)):
+                if s1[i] != s2[i]:
+                    if diffByOne:
+                        return False
+                    diffByOne = True
+            return diffByOne
+        
+        if beginWord == endWord:
+            return 0
+        def construct_dict(word_list):
+            d = {}
+            for word in word_list:
+                for i in range(len(word)):
+                    s = word[:i] + "_" + word[i+1:]
+                    d[s] = d.get(s, []) + [word]
+            return d
+        
+        graph = construct_dict(wordList)
+        
+        return bfs(beginWord, endWord)
+
+
+
+
             
