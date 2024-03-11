ed77983c5be5f2e337ac2e78f279fa860505a357
Indika <45280177+IndiW@users.noreply.github.com>
2024-03-11 08:30:23 -0400

    Update graphProblems.py

--- Graphs/graphProblems.py
+++ Graphs/graphProblems.py
@@ -591,3 +591,34 @@ class Solution:
                     heapq.heappush(vertices, (distance(points[i], points[v]),(v,i)))
         
         return ans
+
+
+'''
+You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+
+We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+
+'''
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # dijsktra
+        '''
+    Create a set sptSet (shortest path tree set) that keeps track of vertices included in the shortest path tree, i.e., whose minimum distance from the source is calculated and finalized. Initially, this set is empty. 
+    Assign a distance value to all vertices in the input graph. Initialize all distance values as INFINITE. Assign the distance value as 0 for the source vertex so that it is picked first. 
+    While sptSet doesn’t include all vertices 
+        Pick a vertex u that is not there in sptSet and has a minimum distance value. 
+        Include u to sptSet. 
+        Then update the distance value of all adjacent vertices of u. 
+            To update the distance values, iterate through all adjacent vertices. 
+            For every adjacent vertex v, if the sum of the distance value of u (from source) and weight of edge u-v, is less than the distance value of v, then update the distance value of v. 
+
+        '''
+        q = []
+        # k is source, so add it first
+        q.append((0,k))
+        while len(ret) < n:
+            return 1
+
+
+
