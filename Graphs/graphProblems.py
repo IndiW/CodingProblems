ce48b68e60a1ab17d9ee96ba0356c882cbb12fef
Indika <45280177+IndiW@users.noreply.github.com>
2024-03-13 16:50:08 -0400

    Update graphProblems.py

--- Graphs/graphProblems.py
+++ Graphs/graphProblems.py
@@ -635,7 +635,21 @@ class Solution:
                     heapq.heappush(heap, (time+w,v))
         return -1
 
+'''
+You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
 
+The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+'''
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # if grid[i][j] = n and we start at elevation 0, we have to wait
+        # for n until we can swim to (i,j)
+        # swim until we can't anymore
+        # get the minimum wait time and try that path
+        # recurse until we reach n-1,n-1
 
 
 
