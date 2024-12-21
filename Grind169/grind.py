e33479d0e710587b618ed7bd3ab4c00d32598b73
Indika <45280177+IndiW@users.noreply.github.com>
2024-12-21 11:29:38 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -141,3 +141,18 @@ class Solution:
         
         return ans
 
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+
+    def dfs(self, nums, target, curr_combo, ret):
+        if target < 0:
+            return
+        if target == 0:
+            ret.append(curr_combo)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], curr_combo+[nums[i]], ret)
+                
