21f77ad494b472b79b0aa73030b428cec07a6f88
Indika <45280177+IndiW@users.noreply.github.com>
2025-01-13 09:26:07 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -747,3 +747,26 @@ class Solution:
         curr.next = l1 or l2
         return dummy.next
             
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        
+        return False
+    
