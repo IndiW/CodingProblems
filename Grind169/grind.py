93c4f973625e1d3700f0d769dec6e379e9983edc
Indika <45280177+IndiW@users.noreply.github.com>
2025-01-13 09:48:11 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -786,3 +786,18 @@ class Solution:
             head = tmp
         
         return prev
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
