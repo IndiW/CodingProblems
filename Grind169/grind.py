6c3d45d527363228064064c1c1ac6cad99a734cf
Indika <45280177+IndiW@users.noreply.github.com>
2025-01-23 21:45:50 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1062,8 +1062,45 @@ class Solution:
         
 
         
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # find middle of list
+        if not head or not head.next:
+            return head
+        fast = head.next
+        slow = head
 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
+        mid = slow.next
+        slow.next = None
+        l, r = self.sortList(head), self.sortList(mid)
+        return self.merge(l, r)
+
+    
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        dummy = p = ListNode(0)
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        
+        p.next = l or r
+        return dummy.next
+
 
 
 
