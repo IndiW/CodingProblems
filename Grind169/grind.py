a721415364934be8f1396703228ebdbb33bb4643
Indika <45280177+IndiW@users.noreply.github.com>
2025-01-27 23:30:01 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1221,7 +1221,29 @@ class Solution:
 
 
             
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        longest = 0
 
+        l = 0
+        r = 0
+        count = 0
+
+        while r < len(nums):
+            if nums[r] == 0:
+                count += 1
+                while count > k:
+                    if nums[l] == 0:
+                        count -= 1
+                    l += 1
+            longest = max(longest, r - l + 1)
+            r += 1
+        
+        return longest
+
+
+
+            
 
 
 
