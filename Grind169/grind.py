92ab2b6e7d747fd195b67c8f4d409ba6ad542c6f
Indika <45280177+IndiW@users.noreply.github.com>
2025-02-15 19:23:35 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1532,3 +1532,26 @@ class Solution:
                         ret.append([ind, i])
     
         return ret
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = [root]
+        while q:
+            node = q.pop()
+            if not node:
+                continue
+            left = node.left
+            right = node.right
+            node.left = right
+            q.append(right)
+            node.right = left
+            q.append(left)
+        
+        return root
