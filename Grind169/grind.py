d81579303a0dca6752bae34a8ffc09d5b4114cc9
Indika <45280177+IndiW@users.noreply.github.com>
2025-02-15 19:36:30 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1555,3 +1555,28 @@ class Solution:
             q.append(left)
         
         return root
+    
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def getDepth(node):
+            if not node:
+                return 0
+            
+            return max(getDepth(node.left), getDepth(node.right)) + 1
+        
+        if not root:
+            return True
+        
+        left = getDepth(root.left)
+        right = getDepth(root.right)
+
+        return abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+        
+
