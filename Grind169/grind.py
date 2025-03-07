ebdffe88b79af45db3d28f48960f88009ecceeb8
Indika <45280177+IndiW@users.noreply.github.com>
2025-03-06 22:10:32 -0500

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1820,5 +1820,13 @@ class Solution:
             
 
             
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
 
