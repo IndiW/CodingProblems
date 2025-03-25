7c5b6e1613655ea3be938afa0bad280746a4b16a
Indika <45280177+IndiW@users.noreply.github.com>
2025-03-25 19:18:25 -0400

    Update grind.py

--- Grind169/grind.py
+++ Grind169/grind.py
@@ -1843,12 +1843,70 @@ class Solution:
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # todo
+        if not root:
+            return 0
+        q = deque()
+        # node, level, index
+        q.append((root, 1, 1))
+        currLevel = 1
+        startIndex = 1
+        ret = 1
+
+        while q:
+            node, level, ind = q.popleft()
+            if not node:
+                continue
+            if level > currLevel:
+                startIndex = ind
+                currLevel = level
+                # in a binary heap array, child indexes are 2*ind and 2*ind+1 indexes away from their parent
+            ret = max(ret, ind - startIndex + 1)
+            if node.left:
+                q.append((node.left, level+1, 2*ind))
+            if node.right:
+                q.append((node.right, level+1, 2*ind + 1))
+        
+        return ret
+        
+
+        
 
 
-        # check
+
+                
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        q = deque()
+        initialLevel = 0
+        q.append([root, initialLevel])
+        levels = [[]]
+
+        while q:
+            node, level = q.popleft()    
+            if node:
+                if level >= len(levels):
+                    levels.append([])
+                if level % 2 == 0:
+                    levels[-1].append(node.val)
+                else:
+                    levels[-1].insert(0, node.val)
+                q.append((node.left, level+1))
+                q.append((node.right, level+1))
+        
+        return levels
 
 
 # The isBadVersion API is already defined for you.
